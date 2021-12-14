21a69fc5717421a833d3ea42d45d04ff34934ed1
pindowcode <95245615+pindowcode@users.noreply.github.com>
2021-12-14 20:47:53 +0300

    Create letters.py

---
+++ letters.py
@@ -0,0 +1,7 @@
+letter=(input("введите букву латинского алфавита "))
+if letter=="a" or letter=="e" or letter=="i" or letter=="o" or letter=="u":
+  print("буква гласная")
+elif letter=="y":
+  print("буква может быть гласной и согласной")
+else:
+  print("буква согласная")
